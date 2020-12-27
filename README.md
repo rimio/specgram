a5d47811b1d7d7088e4b827a10a8401a72a80e92
Vasile Vilvoiu <vasi.vilvoiu@gmail.com>
2020-12-27 13:41:45 +0200

    README stuff

--- README.md
+++ README.md
@@ -3,10 +3,4 @@ Flexible live spectrogram tool.
 
 ## Colormaps
 
-![jet](resources/readme_images/colormap_jet.png?raw=true "Jet colormap")
-![gray](resources/readme_images/colormap_gray.png?raw=true "Gray colormap")
-![purple](resources/readme_images/colormap_purple.png?raw=true "Purple colormap")
-![blue](resources/readme_images/colormap_blue.png?raw=true "Blue colormap")
-![green](resources/readme_images/colormap_green.png?raw=true "Green colormap")
-![orange](resources/readme_images/colormap_orange.png?raw=true "Orange colormap")
-![red](resources/readme_images/colormap_red.png?raw=true "Red colormap")
+![colormaps](resources/readme_images/colormaps.png?raw=true "Colormaps")
--- resources/readme_images/generate.sh
+++ resources/readme_images/generate.sh
@@ -7,7 +7,14 @@ specgram -eqz --bg-color=00000000 --fg-color=000000ff -i ../clips/norah_jones_do
 specgram -eqz --bg-color=00000000 --fg-color=000000ff -i ../clips/norah_jones_dont_know_why colormap_orange.png -c orange
 specgram -eqz --bg-color=00000000 --fg-color=000000ff -i ../clips/norah_jones_dont_know_why colormap_red.png -c red
 
-for f in colormap_*.png
-do
-	convert -geometry 300x ${f} ${f}
-done
+montage \
+-label "jet" colormap_jet.png \
+-label "gray" colormap_gray.png \
+-label "purple" colormap_purple.png \
+-label "blue" colormap_blue.png \
+-label "green" colormap_green.png \
+-label "orange" colormap_orange.png \
+-label "red" colormap_red.png \
+-geometry 200x colormaps.png
+
+rm -rf colormap_*.png
