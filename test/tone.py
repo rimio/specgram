8c762b87124c70ac0c575afb8113a2ce014da3ad
Vasile Vilvoiu <vasi@vilvoiu.ro>
2021-07-15 18:28:04 +0300

    Update e-mail address in copyright notices, README and manpage.
    Closes #14.

--- test/tone.py
+++ test/tone.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020-2021 Vasile Vilvoiu <vasi.vilvoiu@gmail.com>
+# Copyright (c) 2020-2021 Vasile Vilvoiu <vasi@vilvoiu.ro>
 #
 # specgram is free software; you can redistribute it and/or modify
 # it under the terms of the MIT license. See LICENSE for details.
